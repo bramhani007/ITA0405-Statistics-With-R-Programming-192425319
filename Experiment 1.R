825e187991089d1ffe4698883515ed9516049f45
Bramhani Chowdary <bramhanichowdary2007@gmail.com>
2026-07-16 09:07:49 +0530

    Add files via upload

---
+++ Experiment 1.R
@@ -0,0 +1,11 @@
+num=c(10,20,30)
+print(class(num))
+print(num)
+
+char=c("Apple","Banana","Mango")
+print(class(char))
+print(char)
+
+log=c(TRUE,FALSE,TRUE)
+print(class(log))
+print(log)
---
+++ Experiment 6.R
@@ -0,0 +1,6 @@
+x=c(1,2,3,4)
+y=matrix(1:6,nrow=2)
+z=function(x) x^2
+
+mylist=list(Vector=x,Matrix=y,Function=z)
+print(mylist)
